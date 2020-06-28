d1084e74d104346e80b43953f00cb87d50cfc7d0
Ganta Poojitha <poojithaganta95@gmail.com>
2020-06-28 11:28:42 +0530

    DAY 1

---
+++ Day_1_Challenge.java
@@ -0,0 +1,16 @@
+package learncodeonline;
+
+import java.util.*;
+public class day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a string");
+		int digit = sc.nextInt();
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(digit + " * " + i + " = " + digit*i);
+		}
+	}
+
+}
